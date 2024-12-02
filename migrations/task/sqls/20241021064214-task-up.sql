94bafe7c6f9cad435f1da04aac902c31fc0fe73b
MaggieC <honeykiss1216@gmail.com>
2024-12-03 00:52:23 +0800

    fixed 2-1 error

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -50,7 +50,7 @@ SELECT COUNT(*) FROM "USER";
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
 
 SELECT * FROM "USER"
-limit 3
+limit 3;
 
 --  ████████  █████   █    ████  
 --    █ █   ██    █  █         █ 
