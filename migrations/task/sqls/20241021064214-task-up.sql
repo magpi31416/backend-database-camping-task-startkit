684204a2b8c8e91fd6351bbe90ffaa45dd24f531
MaggieC <honeykiss1216@gmail.com>
2024-12-02 22:03:27 +0800

    update 1-2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -24,9 +24,20 @@ values
   ('好野人','richman@hexschooltest.io','USER'),
   ('Q太郎','starplatinum@hexschooltest.io','USER'),
   ('透明人','opacity0@hexschooltest.io','USER');
-  
+
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
+UPDATE "USER"
+SET ROLE = 'COACH'
+WHERE email = 'lee2000@hexschooltest.io';
+
+UPDATE "USER"
+SET ROLE = 'COACH'
+WHERE email = 'muscle@hexschooltest.io';
+
+UPDATE "USER"
+SET ROLE = 'COACH'
+WHERE email = 'starplatinum@hexschooltest.io';
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
