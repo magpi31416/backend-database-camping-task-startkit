1ee90d186ec6ca34af5ce2e93efcdd3bb2716099
MaggieC <honeykiss1216@gmail.com>
2024-12-04 11:19:54 +0800

    update 3-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -160,6 +160,18 @@ limit 3;
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
     -- 2. 教練`Q太郎` 的經驗年數為5年
 
+--3-3.1
+        update "COACH" 
+        set experience_years = 3 
+        where user_id = (
+            select id from "USER" 
+            where email = 'muscle@hexschooltest.io');
+--3-3.2
+        update "COACH" 
+        set experience_years = 5 
+        where user_id = (
+            select id from "USER" 
+            where email = 'starplatinum@hexschooltest.io');
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
 
 
